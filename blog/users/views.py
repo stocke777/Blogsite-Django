54258beebf143e24854cc2e36eb12f32383680c4
stocke777 <deshwaljaivardhan@gmail.com>
2020-06-04 10:57:19 +0530

    updates

---
+++ blog/users/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from .forms import UserRegisterForm, UpdateUserForm, UpdateProfileForm
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+
+# Create your views here.
+
+def register(request):
+    if request.method=="POST":
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f"Account Created for {username}!!!")
+            return redirect("home")
+    else:
+        form = UserRegisterForm()
+    return render(request, 'users/register.html', {'form':form})
+
+@login_required
+def profile(request):
+    if request.method == "POST":
+        u_form = UpdateUserForm(request.POST, instance=request.user)
+        p_form = UpdateProfileForm(request.POST, request.FILES, instance=request.user.profile)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, f"Profile Updated!!!")
+            return redirect('profile')
+    else:
+        u_form = UpdateUserForm(instance=request.user)
+        p_form = UpdateProfileForm(instance=request.user.profile)
+    context = {
+        'u_form':u_form,
+        'p_form':p_form
+    }
+    return render(request, 'users/profile.html', context)
